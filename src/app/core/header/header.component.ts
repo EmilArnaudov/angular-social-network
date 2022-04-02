1d4e4dd11162abf8bde9c1e9bd6a88a1401141e3
EmilArnaudov <emil.arnaudov.ecom@gmail.com>
2022-04-02 01:46:11 +0100

    add user search by username

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { AuthService } from 'src/app/auth/auth.service';
+import { SearchService } from '../search.service';
 
 @Component({
   selector: 'app-header',
@@ -15,18 +17,29 @@ export class HeaderComponent implements OnInit{
 
    get username() {
      return localStorage.getItem('<USERNAME>');
-   } 
+   }
+
+  customValue: any = ''; 
 
    form!: FormGroup;
    results: any[] = [];
    queryField: FormControl = new FormControl();
 
-  constructor(private authService: AuthService, private renderer: Renderer2) { }
+
+  constructor(private authService: AuthService, private renderer: Renderer2, private searchService: SearchService) { }
 
   ngOnInit(): void {
     this.queryField.valueChanges
-      .subscribe(result => console.log(result)
-      )
+    .pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((query: string) => this.searchService.searchQuery(query))
+    )
+    .subscribe(users => this.results = users)
+  }
+
+  clearSearchBar() {
+
   }
 
   logout() {
--- src/app/core/search.service.ts
+++ src/app/core/search.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { collection, Firestore, getDocs, QuerySnapshot, DocumentData } from '@angular/fire/firestore';
+import { from, map, mergeMap, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  constructor() { }
-}
+  constructor(private firestore: Firestore) { }
+
+  searchQuery(query: string): Observable<any> {
+    if (!query) {
+      return from([]);
+    }
+    return from(getDocs(collection(this.firestore, 'users'))
+      .then(snapshot => {
+        let arr: any[] = [];
+        snapshot.forEach(docData => {
+          let user = docData.data();
+          if (user['username'].toLowerCase().includes(query.toLowerCase())) {
+            arr.push(user);
+          }
+        })
+        return arr;
+      }))
+
+
+     
+}}
